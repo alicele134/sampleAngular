efc9872b310f8791382fe2a2948e1beb978977e9
Hanh Le <hanh.le@intl.opswat.com>
2019-03-18 16:56:46 +0700

    push all

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
-    $scope.name = "John Doe";
+var packages = [
+    {
+        "packageName": "abc", 
+        "description": "this is example",
+        "group": "1",
+        "lastUpdate": "Mar 15, 2019 09:45:44 AM"
+    },
+    {
+        "packageName": "xyz", 
+        "description": "this is example",
+        "group": "1",
+        "lastUpdate": "Mar 15, 2019 09:45:44 AM"
+    },
+    {
+        "packageName": "bcn", 
+        "description": "this is example",
+        "group": "1",
+        "lastUpdate": "Mar 15, 2019 09:45:44 AM"
+    }
+];
+
+var app = angular.module("myApp", []);
+app.controller("myController", function($scope) {
+    $scope.list = packages;
+
 })
+
