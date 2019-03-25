88aed125f39433d8bdf8d9f065ede9f64f0c2842
Hanh Le <hanh.le@intl.opswat.com>
2019-03-25 17:34:04 +0700

    sap xong

--- main.js
+++ main.js
@@ -27,26 +27,131 @@ var packages = [
 
 var app = angular.module("myApp", []);
 
-// app.config(function($routeProvider) {
-//     $routeProvider
-//     .when('/', {
-//         controller:"myController",
-//         templateUrl: "rebranding.html"
-        
-//     })
-//     .when('/add-new-package', {
-//         templateUrl: "addNewPakage.html",
-//         controller: "addNewPackageCtrl"
-//     })
-// })
-
-
 app.controller("myController", function($scope) {
-    $scope.showMeNewPackage = false;
-    $scope.showMeTable = true;
-    $scope.list = packages;
+    $scope.showMeNewPackage = false;        // Khởi tạo ban đầu
+    $scope.showMeTable = true;              // Khởi tạo ban đầu
+    $scope.showMeEditPackage = false;          // Khởi tạo ban đầu
+    $scope.list = packages;                 // List package
+    $scope.btnAddShow = true;               // Change của button Add
+    var indexArr = [];                      // các vị trí cần xóa sẽ lưu vào đây
+    var empty = "define not yet";
+    $scope.descriptionShow = "";
+
+    // edit
+    $scope.showNamePack = true;
+    $scope.showEditNamePack = false;
+    $scope.showDescriptionPack = true;
+    $scope.showEditDescriptionPack = false;
+
+    // show page "add new package"
     $scope.addNewPackage = function() {
-        $scope.showMeNewPackage = !$scope.showMeNewPackage;
-        $scope.showMeTable = !$scope.showMeTable ;
+        [$scope.showMeTable, $scope.showMeNewPackage] = [false, true];
+    }
+
+    // Thêm mới package
+    $scope.addNewPackFunc = function() { 
+        var newPack = {
+            packageName : $scope.namePack,
+            description: $scope.discription, 
+            group: 2, 
+            lastUpdate: "Mar 22, 2019 09:21:00 AM"
+        }
+
+        packages.push(newPack);
+
+        // Show page "rebranding"
+        [$scope.showMeTable, $scope.showMeNewPackage, $scope.showMeEditPackage] = [true, false, false];
+
+        $scope.namePack = "";
+        $scope.discription = "";
+        $scope.btnAddShow = true;  
+    }
+
+    // Thay đổi thuộc tính hiển thị button Add
+    $scope.valueChange = function(){
+        var namePackage = $scope.namePack;
+        if (namePackage.length === 0) {
+            $scope.btnAddShow = true;
+        }
+        $scope.btnAddShow = false;
+        //$applyAsync(); // ???????????????????????????????????????
+        
+    }
+
+    // Sự kiện khi click vào buttn Discard
+    $scope.discardFunc = function () {
+      [$scope.showMeTable, $scope.showMeNewPackage, $scope.showMeEditPackage] = [true, false, false]
+    }
+
+    // Xóa Package
+    $scope.deleteFunc = function() {
+        for (var i in indexArr) {
+            packages.splice(indexArr[i],1);
+        }
+        indexArr.splice(0, indexArr.length);
+    }
+
+    // Push phần tử cần xóa vào mảng indexArr
+    $scope.isSelected = function(i) { 
+        indexArr.push(i);
+    }
+
+    /////////////////  EDIT  ////////////////////
+    $scope.editPackagePage = function(index) {
+        $scope.index = index;
+        $scope.showMeTable = false;
+        $scope.showMeNewPackage = false;
+        $scope.showMeEditPackage = true;
+      //  alert("shdf: " + $scope.descriptionShow)
+        $scope.namePack = packages[index].packageName;
+        var des = (packages[index].description !== "") ? packages[index].description : empty;
+        $scope.descriptionShow = des;
+       // alert("shdf: " + $scope.descriptionShow)
+    }
+
+    var status = 0;
+
+    // hiển thị input change namePack
+    $scope.editName = function() {
+        $scope.showNamePack = false;
+        $scope.showEditNamePack = true;
+    }
+
+    // Change Name Pack
+    $scope.changeNamePack = function() {
+        $scope.showNamePack = true;
+        $scope.showEditNamePack = false;
+       
+    }
+
+    // hiển thi input change Description
+    $scope.EditDescriptionPack = function() {
+        $scope.showDescriptionPack = false;
+        $scope.showEditDescriptionPack = true;
+    }
+
+    //change Description Pack
+    $scope.changeDescriptionPack = function() {
+        $scope.showDescriptionPack = true;
+        $scope.showEditDescriptionPack = false;
+    }
+
+    // Edit package khi click button Change
+    $scope.editPackage = function() {
+        alert ($scope.index)
+
+        var p = {
+            packageName: $scope.namePack,
+            description: $scope.descriptionShow,
+            group: "2",
+            lastUpdate: "Mar 15, 2019 09:45:44 AM"
+        }
+
+        packages[$scope.index] = p;
+        [$scope.showMeTable, $scope.showMeNewPackage, $scope.showMeEditPackage] = [true, false, false];
+    }
+
+    $scope.orderByMe = function(x) {
+        $scope.myOrderBy = x;
     }
 });
